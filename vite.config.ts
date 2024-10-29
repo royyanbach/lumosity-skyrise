7f2a8f33d6cc1fb947dcf712378d9e2810709d08
royyanbach <royyanbach@users.noreply.github.com>
2024-10-30 02:13:03 +0700

    Update asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    server: {
-        host: true,
-        port: 3000,
-    },
-    build: {
-        target: 'esnext',
-        sourcemap: true,
-    }
+export default defineConfig(({ command, mode }) => {
+    return {
+        base: command === 'build' ? '/lumosity-skyrise/' : '/',
+        server: {
+            host: true,
+            port: 3000,
+        },
+        build: {
+            target: 'esnext',
+            sourcemap: true,
+        }
+    };
 });
